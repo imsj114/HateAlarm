a444763ddf2431a7cbbed56c7720aa338ea09a8e
EunjiHong <eunjihong.3698@gmail.com>
2020-07-16 23:13:04 +0900

    Contacts Tab floating buttons implm

--- app/src/main/java/com/example/madcampweek2/ui/contact/ContactFragment.java
+++ app/src/main/java/com/example/madcampweek2/ui/contact/ContactFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.madcampweek2.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,10 +23,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
-public class ContactFragment extends Fragment {
+public class ContactFragment extends Fragment implements View.OnClickListener{
 
     private RecyclerAdapter adapter;
-
+    private FloatingActionButton fab_main, fab_sub1, fab_sub2;
+    private Animation fab_open, fab_close;
+    private boolean isFabOpen = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -31,12 +37,22 @@ public class ContactFragment extends Fragment {
         final androidx.recyclerview.widget.RecyclerView ContactView =
                 root.findViewById(R.id.recyclerview_contacts);
 
+        fab_open = AnimationUtils.loadAnimation(getActivity(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getActivity(), R.anim.fab_close);
+
+        fab_main = (FloatingActionButton) root.findViewById(R.id.fab_main);
+        fab_sub1 = (FloatingActionButton) root.findViewById(R.id.fab_sub1);
+        fab_sub2 = (FloatingActionButton) root.findViewById(R.id.fab_sub2);
+
+        fab_main.setOnClickListener(this);
+        fab_sub1.setOnClickListener(this);
+        fab_sub2.setOnClickListener(this);
+
         // RecyclerView setting
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         ContactView.setLayoutManager(linearLayoutManager);
         adapter = new RecyclerAdapter();
         ContactView.setAdapter(adapter);
-
 
         // Recycler adapter contact data setting
         setJSONcontacts(adapter, "contacts.json");  // Load contact data from contacts.json
@@ -44,6 +60,54 @@ public class ContactFragment extends Fragment {
         adapter.notifyDataSetChanged(); // Notify the adapter data modification
 
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab_main:
+                switchFab();
+                break;
+            case R.id.fab_sub1:
+                switchFab();
+                Toast.makeText(getActivity(), "First Fab", Toast.LENGTH_SHORT).show();
+                /*
+                * Do something
+                */
+                break;
+            case R.id.fab_sub2:
+                switchFab();
+                Toast.makeText(getActivity(), "Second Fab", Toast.LENGTH_SHORT).show();
+                /*
+                 * Do something
+                 */
+                break;
+        }
+    }
+
+    // Fab open/close switch
+    private void switchFab() {
+        if (isFabOpen) {
+            fab_main.setImageResource(R.drawable.ic_baseline_add_circle_24);
+
+            fab_sub1.startAnimation(fab_close);
+            fab_sub2.startAnimation(fab_close);
+
+            fab_sub1.setClickable(false);
+            fab_sub2.setClickable(false);
+
+            isFabOpen = false;
+        } else {
+            fab_main.setImageResource(R.drawable.ic_baseline_cancel_24);
+
+            fab_sub1.startAnimation(fab_open);
+            fab_sub2.startAnimation(fab_open);
+
+            fab_sub1.setClickable(true);
+            fab_sub2.setClickable(true);
+
+            isFabOpen = true;
+        }
     }
 
 
@@ -93,7 +157,6 @@ public class ContactFragment extends Fragment {
     }
 
     // Load the user's contact list
-    // put
     public void setDeviceContacts(RecyclerAdapter adapter){
         ContactUtil contactUtil = new ContactUtil(getActivity());
 
